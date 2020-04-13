51994f74b46f96ed7e4ee0387e30a0fb6758f5c8
Guosa Edebiri <gedebiri@gmail.com>
2020-04-13 14:48:40 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -34,8 +34,6 @@ def sorted_matrix
     ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"].sort, 
     ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"].sort
     ]
-    
-  
   
 end
 
@@ -51,4 +49,7 @@ def matrix_update(matrix, row, column, new_value)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
+  
+  
+  
 end
